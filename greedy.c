b0bf64b8c5a8f3b162ade453e459124229adf2d4
Deepank Chauhan <deepankchauhan73@gmail.com>
2019-01-16 02:49:18 +0530

    Gives the change
    Gives the least number of coins in which the change can be given

---
+++ greedy.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int main(void)
+{
+    float change;
+    int a = 0, b = 0, c = 0, d = 0;
+
+    // this prompts the user to give an input which can have floating point.
+    do
+    {
+        change = get_float("O hai! How much change is owed?\n");
+    }
+    while (change < 0);
+
+    int cents = (int)round(change * 100);
+
+
+
+    // logic
+    // checks the input, if greater or equal to 25, then it divides it from 25 else the loop breaks.
+    while (cents >= 25)
+    {
+        a = cents / 25;
+        break;
+    }
+
+    // checks the previous result (a), if greater or equal to 10 and smaller than 25, then it divides it from 10 else the loop breaks.
+    while ((cents % 25) >= 10 && (cents % 25) < 25)
+    {
+        b = (cents % 25) / 10;
+        break;
+    }
+
+    // checks the previous result (b), if greater or equal to 5 and smaller than 10, then it divides it from 5 else the loop breaks.
+    while (((cents % 25) % 10) >= 5 && ((cents % 25) % 10) < 10)
+    {
+        c = ((cents % 25) % 10) / 5;
+        break;
+    }
+
+    // checks the previous result (c), if less than 5, then it divides it from 1 else the loop breaks.
+    while ((((cents % 25) % 10) % 5) < 5)
+    {
+        d = (((cents % 25) % 10) % 5) / 1;
+        break;
+    }
+
+    // this prints the final output.
+    printf("%i\n", a + b + c + d);
+    return 0;
+}
